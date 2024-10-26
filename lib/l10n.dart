5ce49ad545b1e3550135751523b96e75d502204e
enzo-viguier <enzoviguier@outlook.fr>
2024-10-26 05:21:49 +0200

    Fichier de gestion de la traduction

---
+++ lib/l10n.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:logger/logger.dart';
+
+class AppLocalizations {
+  final Locale locale;
+  
+  Logger logger = Logger();
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate = _AppLocalizationsDelegate();
+
+  Map<String, String> _localizedStrings = {};
+
+  Future<bool> load() async {
+    String jsonString =
+    await rootBundle.loadString('l10n/app_${locale.languageCode}.json');
+
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    _localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+
+    return true;
+  }
+
+  String translate(String key) {
+    return _localizedStrings[key] ?? key;
+  }
+
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) => ['en', 'fr'].contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
